dbac5e432fe0f3584a698f1424135c52e938816b
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-07-22 22:20:29 +0300

    feat: create header for signin page

---
+++ src/entities/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <p className='header__logo'>easyfit</p>
+      <h1 className='header__title'>вход</h1>
+    </div>
+  )
+}
+
+export default Header
