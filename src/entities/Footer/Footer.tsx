c3e18be68182be18ad37fcfc7e1d6a803a4fbe18
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-07-22 22:20:11 +0300

    feat: created footer for signin page

---
+++ src/entities/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import './Footer.css'
+export const Footer = () => {
+  return (
+    <div className="signin__footer">
+      <p className="signin__footer-text">Еще нет аккаунта? <span className="signin__footer-span">Создать</span></p>
+    </div>
+  )
+}
