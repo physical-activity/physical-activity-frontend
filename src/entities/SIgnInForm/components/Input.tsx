fb460d8456c58a001306a1282116f1d0a4601bb0
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-07-22 22:43:06 +0300

    fix: image fix

--- src/entities/SIgnInForm/components/Input.tsx
+++ src/entities/SIgnInForm/components/Input.tsx
@@ -5,7 +5,7 @@ export const Input = () => {
   return (
     <div className="signin__input-block">
       <input className="signin__input"/>
-      <p className="signin__input-name">Почта <img src={requiredImg} className="signin__input-span" alt='required'/></p>
+      <p className="signin__input-name">Почта <img src={require('./ic_required_1.svg').default} className="signin__input-span" alt='required'/></p>
       <button className="signin__input-button" />
     </div>
   )
