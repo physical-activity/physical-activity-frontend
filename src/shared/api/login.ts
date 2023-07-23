aca1657eaa3382fc0d0c1de3d208f5558e8833a3
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-07-23 21:25:47 +0300

    feat: api call to backend
    post backend request for singing in

---
+++ src/shared/api/login.ts
@@ -0,0 +1,21 @@
+const BASE_USL = process.env.REACT_APP_BACKEND_BASE_URL
+console.log(BASE_USL)
+export const signin = async (email: string, password: string) => {
+  const response = await fetch(`${BASE_USL}/auth/token/login`, {
+    method: 'POST',
+    headers: {
+      "Content-Type": 'application/json'
+    },
+    body: JSON.stringify({
+      "password": password,
+      "email": email
+    })
+  })
+  let res
+  if (response.status === 200) {
+    res = response.json()
+  } else {
+    throw new Error('Smth went wrong')
+  }
+  return res
+}
