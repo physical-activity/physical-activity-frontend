8b79de29a2a38a3979fed4aa19c19f677428d9b0
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-07-24 10:49:36 +0300

    fix: changed Input component className

--- src/entities/Input/Input.tsx
+++ src/entities/Input/Input.tsx
@@ -33,32 +33,32 @@ export const Input = ({
 
 
 	return (
-		<div className="signin__input-block">
+		<div className="input-block">
 			<input
 				required
 				name={name}
 				type={visibility}
 				value={value}
-				className={`signin__input ${isValidInput && 'signin__input_error'}`}
+				className={`input-block__input ${isValidInput && 'signin__input_error'}`}
 				onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e)}
 				pattern={pattern}
 			/>
 			{!value && (
-				<p className="signin__input-name">
+				<p className="input-block__input-name">
 					{placeholder}{' '}
 					<img
 						src={require('./ic_required_1.svg').default}
-						className="signin__input-span"
+						className="input-block__input-span"
 						alt="required"
 					/>
 				</p>
 			)}
 			{isValidInput!=='' && (
-				<span className="signin__input-error">{isValidInput}</span> 
+				<span className="input-block__input-error">{isValidInput}</span> 
 			)}
 			{type === 'password' && (
 				<img
-					className="signin__input-hidebutton"
+					className="input-block__input-hidebutton"
 					src={
 						require('./visibility_off_FILL0_wght300_GRAD0_opsz48 1.svg').default
 					}
