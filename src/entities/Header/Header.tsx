e678067ba078449736b1846a8a1635dba9c330df
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-07-27 19:53:16 +0300

    fix: ehader comp changes

--- src/entities/Header/Header.tsx
+++ src/entities/Header/Header.tsx
@@ -2,7 +2,6 @@ import './Header.css';
 const Header = ({ name }: { name: string }) => {
 	return (
 		<div className="header">
-			{/* <p className="header__logo">easyfit</p> */}
 			<h2 className="header__title">{name}</h2>
 		</div>
 	);
