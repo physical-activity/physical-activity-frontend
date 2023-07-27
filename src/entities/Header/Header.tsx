35206909165659d99d5e76a440d6c995c797f313
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-07-27 13:24:44 +0300

    feat: changed header
    delete logo from header

--- src/entities/Header/Header.tsx
+++ src/entities/Header/Header.tsx
@@ -2,8 +2,8 @@ import './Header.css';
 const Header = ({ name }: { name: string }) => {
 	return (
 		<div className="header">
-			<p className="header__logo">easyfit</p>
-			<h1 className="header__title">{name}</h1>
+			{/* <p className="header__logo">easyfit</p> */}
+			<h2 className="header__title">{name}</h2>
 		</div>
 	);
 };
