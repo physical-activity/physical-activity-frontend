70daf65c71e0c103c36d482d8218277bef41732f
Vladislav Pavlov <vldslw@gmail.com>
2023-08-07 05:57:48 -0300

    feat: adjust header for registration pages

--- src/entities/RegisterHeader/RegisterHeader.tsx
+++ src/entities/RegisterHeader/RegisterHeader.tsx
@@ -1,8 +1,13 @@
 import './RegisterHeader.css';
 const RegisterHeader = ({ name }: { name: string }) => {
 	return (
-		<div className="register-header">
-			<h2 className="register-header__title">{name}</h2>
+		<div>
+			<div className="register-header">
+				<h1 className="register-header__logo">easyfit</h1>
+			</div>
+			<div>
+				<h2 className="register-header__title">{name}</h2>
+			</div>
 		</div>
 	);
 };
