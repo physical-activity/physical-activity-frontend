5290f24f5f2ef7f5ffd0cadd7fba284be2046380
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-08-11 17:04:35 +0300

    feat: api call change user info

---
+++ src/shared/api/changeUserInfo.tsx
@@ -0,0 +1,28 @@
+const BASE_USL = process.env.REACT_APP_BACKEND_BASE_URL;
+console.log(BASE_USL);
+export const changeUserInfo = async (
+	name: string,
+	secondName: string,
+	email: string
+) => {
+	const token = localStorage.getItem('token');
+	const response = await fetch(`http://91.201.53.71/api/v1/account/`, {
+		method: 'PATCH',
+		headers: {
+			Authorization: `Token ${token}`,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			first_name: name,
+			last_name: secondName,
+			email: email,
+		}),
+	});
+	let res;
+	if (response.status === 200) {
+		res = response.json();
+	} else {
+		throw new Error('Smth went wrong');
+	}
+	return res;
+};
