e7ae9daa8558329139a6252eda6b496bc3532070
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-08-11 17:05:06 +0300

    feat: api call get user info

---
+++ src/shared/api/getUserData.tsx
@@ -0,0 +1,16 @@
+export const getUserData = async () => {
+	const token = localStorage.getItem('token');
+	const res = await fetch('http://91.201.53.71/api/v1/account/', {
+		method: 'GET',
+		headers: {
+			Authorization: `Token ${token}`,
+		},
+	});
+	let response;
+	if (res.status === 200) {
+		response = res.json();
+	} else {
+		throw new Error('Smth went wrong');
+	}
+	return response;
+};
