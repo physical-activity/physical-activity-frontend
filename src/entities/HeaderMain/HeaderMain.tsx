80cdca24023ad9794ff0fa0ded89a9fc1414f102
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-08-11 17:03:47 +0300

    fix: header main style fixes

--- src/entities/HeaderMain/HeaderMain.tsx
+++ src/entities/HeaderMain/HeaderMain.tsx
@@ -4,23 +4,40 @@ import './HeaderMain.css';
 
 import loginIcon from './icons/login.svg';
 
-const HeaderMain = () => {
+const HeaderMain = ({
+	userData,
+}: {
+	userData: { first_name: string; photo: string };
+}) => {
 	const navigate = useNavigate();
-
+	const token = localStorage.getItem('token');
 	return (
 		<div className="header-main">
 			<h1 className="header-main__logo">easyfit</h1>
 			<div className="header-main__user">
-				<p className="header-main__name">Влад</p>
-				<button
-					className="header-main__btn"
-					onClick={() => {
-						navigate('/users/123');
-					}}
-				>
-					<p className="header-main__letter">B</p>
-					{/* <img src={loginIcon} alt="Логин" /> */}
-				</button>
+				<p className="header-main__name">{userData.first_name}</p>
+				{token !== null ? (
+					<>
+						<button
+							className="header-main__btn"
+							onClick={() => {
+								navigate('/users/123');
+							}}
+						>
+							<p className="header-main__letter">
+								{userData.first_name.slice(0, 1)}
+							</p>
+						</button>
+					</>
+				) : (
+					<>
+						<img
+							src={loginIcon}
+							alt="Логин"
+							onClick={() => navigate('/signin')}
+						/>
+					</>
+				)}
 			</div>
 		</div>
 	);
