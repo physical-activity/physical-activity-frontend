a777a13fc840780fb5d4ead9e6b6d87daaf4f92e
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2023-08-16 00:36:28 +0600

    fix: training page error

--- src/pages/training/index.tsx
+++ src/pages/training/index.tsx
@@ -2,14 +2,16 @@ import './index.css';
 
 import HeaderMain from 'entities/HeaderMain/HeaderMain';
 import TextBlock from 'entities/TextBlock/TextBlock';
-
 import FooterMain from 'entities/FooterMain/FooterMain';
 import { TrainingForm } from 'entities/TrainingForm/TrainingForm';
+import { useAppSelector } from 'shared/hooks/redux';
 
 export const TrainingPageMobile = () => {
+	const userData = useAppSelector((state) => state.user);
+
 	return (
 		<div className="page-container">
-			<HeaderMain />
+			<HeaderMain userData={userData} />
 			<main>
 				<TextBlock text={'тренировка'} />
 				<TrainingForm />
