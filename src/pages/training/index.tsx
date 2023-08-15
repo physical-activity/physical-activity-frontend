e775c6ba2a33e9b6e24e01052d07e69f22009041
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2023-08-15 23:46:47 +0600

    fix: training page error

--- src/pages/training/index.tsx
+++ src/pages/training/index.tsx
@@ -1,6 +1,6 @@
 import './index.css';
 
-import HeaderMain from 'entities/HeaderMain/HeaderMain';
+// import HeaderMain from 'entities/HeaderMain/HeaderMain';
 import TextBlock from 'entities/TextBlock/TextBlock';
 
 import FooterMain from 'entities/FooterMain/FooterMain';
@@ -9,7 +9,7 @@ import { TrainingForm } from 'entities/TrainingForm/TrainingForm';
 export const TrainingPageMobile = () => {
 	return (
 		<div className="page-container">
-			<HeaderMain />
+			{/* <HeaderMain /> */}
 			<main>
 				<TextBlock text={'тренировка'} />
 				<TrainingForm />
