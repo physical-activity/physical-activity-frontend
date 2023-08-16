16a0ed280416f5d996d4032ec051864ccd5c17db
Roman Frolov <frolloff1@yandex.ru>
2023-08-16 11:51:17 +0300

    feat: сделать календарь и стори к нему

---
+++ src/features/calendar/index.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CustomCalendar from '.';
+
+const meta = {
+	title: 'Features/Calendar',
+	component: CustomCalendar,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'fullscreen',
+	},
+} satisfies Meta<typeof CustomCalendar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
