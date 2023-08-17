68ab992f5c4ba86b469e95d74bc100b9c75b5690
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2023-08-18 01:08:04 +0600

    feat: add my training page

--- src/entities/MyTrainings/MyTrainings.tsx
+++ src/entities/MyTrainings/MyTrainings.tsx
@@ -5,7 +5,7 @@ export const MyTrainings = () => {
 	return (
 		<div className="my-trainings">
 			<div className="my-trainings__status">
-				<button className="my-trainings__status-button">План</button>
+				<button className="my-trainings__status-button_active">План</button>
 				<button className="my-trainings__status-button">Пропущено</button>
 				<button className="my-trainings__status-button">Выполнено</button>
 			</div>
