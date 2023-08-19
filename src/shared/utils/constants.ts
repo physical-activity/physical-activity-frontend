c21156d3dbe681f399c930ca1c632b8891c0332c
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-08-19 07:02:24 +0300

    name regexp changes

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-useless-escape */
 export const REGEX = {
+	name: /^[A-Za-zА-Яа-я\s\-]{2,50}$/,
 	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	password: /[a-zA-Z0-9\#\?\!\@\$\%\^\&\*\-]*.{6,}/,
-	name: /^[A-Za-zА-Яа-я\s\-]{2,50}$/,
 	date: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/, // ДД/ММ/ГГГГ
 	time: /^\d{2}[:]\d{2}$/,
 	distance: /[0-9]+/,
