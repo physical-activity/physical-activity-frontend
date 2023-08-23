f9fd59ebc6f73d6e2eaee8e093006621cffc5fd0
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-08-23 17:00:54 +0300

    fix: route protection fix

--- src/shared/hooks/require-auth.tsx
+++ src/shared/hooks/require-auth.tsx
@@ -1,15 +1,11 @@
-import { FC, ReactElement } from 'react';
-import { Navigate, useLocation } from 'react-router';
+import React from 'react';
+import { Navigate, Outlet } from 'react-router';
+import { useAppSelector } from './redux';
 
-const RequireAuth: FC<{ children: ReactElement }> = ({ children }) => {
-	const location = useLocation();
-	const token = localStorage.getItem('token');
+const RequireAuth = () => {
+	const token = useAppSelector((state) => state.user.auth_token);
 
-	return !token ? (
-		<Navigate to="/signin" state={{ from: location }} />
-	) : (
-		children
-	);
+	return !token ? <Navigate to="/signin" /> : <Outlet />;
 };
 
 export default RequireAuth;
