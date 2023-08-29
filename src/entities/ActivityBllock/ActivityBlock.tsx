ef0b425584f36eec208a94a8b0ae1edad57138da
Andrey <101524084+AndreySaveliev@users.noreply.github.com>
2023-08-29 18:11:38 +0300

    feat: add activity data on main page

--- src/entities/ActivityBllock/ActivityBlock.tsx
+++ src/entities/ActivityBllock/ActivityBlock.tsx
@@ -37,21 +37,29 @@ const ActivityBlock = () => {
 							<h4 className={styles.activity__subtitle}>Дистанция</h4>
 							<img src={widthIcon} alt="" />
 							<p className={styles.activity__data}>
-								<span className={styles.activity__number}>{activity.distance / 1000}</span> км
+								<span className={styles.activity__number}>
+									{activity.distance / 1000}
+								</span>{' '}
+								км
 							</p>
 						</div>
 						<div className={styles.activity__item}>
 							<h4 className={styles.activity__subtitle}>Шаги</h4>
 							<img src={stepsIcon} alt="" />
 							<p className={styles.activity__data}>
-								<span className={styles.activity__number}>{activity.steps}</span>
+								<span className={styles.activity__number}>
+									{activity.steps}
+								</span>
 							</p>
 						</div>
 						<div className={styles.activity__item}>
 							<h4 className={styles.activity__subtitle}>Длительность</h4>
 							<img src={timerIcon} alt="" />
 							<p className={styles.activity__data}>
-								<span className={styles.activity__number}>{activity.duration / 60}</span>
+								<span className={styles.activity__number}>
+									{'0' + (activity.duration / 60).toString().slice(-2)} :{' '}
+									{'0' + (activity.duration % 60)}
+								</span>
 							</p>
 						</div>
 					</div>
